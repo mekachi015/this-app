120602788247f78e583e8cb2893889aeb5a11715
mekachi015 <141609950+mekachi015@users.noreply.github.com>
2025-05-22 11:03:57 +0200

    modified store front

--- this-app/src/app/pages/store-front/store-page/store-page.component.ts
+++ this-app/src/app/pages/store-front/store-page/store-page.component.ts
@@ -24,11 +24,68 @@ export class StorePageComponent {
       id: '2',
       name: 'BROKE',
       imageUrl: 'assets/stores/broke.jpg',
+    },
+    {
+      id: '5',
+      name: 'VINTAGE',
+      imageUrl: 'assets/stores/vintage.jpg',
+    },
+    {
+      id: '6',
+      name: 'STREETWEAR',
+      imageUrl: 'assets/stores/streetwear.jpg',
+    },
+    {
+      id: '7',
+      name: 'LUXURY',
+      imageUrl: 'assets/stores/luxury.jpg',
+    },
+    {
+      id: '1',
+      name: 'IFUKU',
+      imageUrl: 'assets/stores/ifuku.jpg',
+    },
+    {
+      id: '2',
+      name: 'BROKE',
+      imageUrl: 'assets/stores/broke.jpg',
+    },
+    {
+      id: '5',
+      name: 'VINTAGE',
+      imageUrl: 'assets/stores/vintage.jpg',
+    },
+    {
+      id: '6',
+      name: 'STREETWEAR',
+      imageUrl: 'assets/stores/streetwear.jpg',
+    },
+    {
+      id: '7',
+      name: 'LUXURY',
+      imageUrl: 'assets/stores/luxury.jpg',
     }
   ];
 
   nearbyStores: Store[] = [
     {
+      id: '3',
+      name: 'Testseller',
+      imageUrl: 'assets/stores/testseller.jpg',
+      bestseller: 'Everything',
+      category: 'Thrifting',
+      rating: 95,
+      operatingHours: '09:00 - 21:00'
+    },
+    {
+      id: '4',
+      name: 'IFUKU',
+      imageUrl: 'assets/stores/ifuku.jpg',
+      bestseller: 'Blue Japanese Denim',
+      category: 'Japanese Denim',
+      rating: 95,
+      operatingHours: '09:00 - 17:00'
+    },{
       id: '3',
       name: 'Testseller',
       imageUrl: 'assets/stores/testseller.jpg',
