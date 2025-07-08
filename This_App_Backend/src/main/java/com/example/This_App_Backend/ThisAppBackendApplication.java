553f021f6eb51bf3dcc3f371633323f4d2fdb4ce
mekachi015 <141609950+mekachi015@users.noreply.github.com>
2025-07-08 10:43:30 +0200

    fixed navigation issues and overall fix

---
+++ This_App_Backend/src/main/java/com/example/This_App_Backend/ThisAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.This_App_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisAppBackendApplication.class, args);
+	}
+
+}
